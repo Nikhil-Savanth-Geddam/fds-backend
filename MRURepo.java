07a01877e9489068775ecdbc5da1a76c91cdde75
Nikhil-Savanth-Geddam <157471586+Nikhil-Savanth-Geddam@users.noreply.github.com>
2025-03-08 11:35:44 +0530

    Create MRURepo.java

---
+++ MRURepo.java
@@ -0,0 +1,9 @@
+package com.mru.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mru.entity.Mobiles;
+
+public interface MRURepo extends JpaRepository<Mobiles, java.lang.Integer> {
+
+}
