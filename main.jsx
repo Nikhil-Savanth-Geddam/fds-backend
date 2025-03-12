62c3fd280d2db434f5a16a24f1ef3f1b04a98556
Nikhil-Savanth-Geddam <157471586+Nikhil-Savanth-Geddam@users.noreply.github.com>
2025-03-12 11:42:44 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Mobiles from './components/Mobiles'
+
+createRoot(document.getElementById('root')).render(
+  
+    <Mobiles/>
+  
+)
